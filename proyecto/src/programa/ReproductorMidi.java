6776da7b99c6e6e2fc5944e93b335280618a61ce
anagg07 <ana24garcia07@gmail.com>
2024-05-08 14:22:03 +0200

    Esqueleto Programa

--- proyecto/src/programa/ReproductorMidi.java
+++ proyecto/src/programa/ReproductorMidi.java
@@ -5,17 +5,17 @@ import java.awt.*;
 public class ReproductorMidi {
 
     // PROPIEDADES
-    private static final Color[] COLORES;
+    private static final Color[] COLORES = new Color[0];
     private Piano piano;
 
     //CONSTRUCTOR
     public ReproductorMidi(){
-
+        throw new UnsupportedOperationException("Metodo no programado");
     }
 
     // MÉTODOS
     public void reproducir(String ruta){
-
+        throw new UnsupportedOperationException("Metodo no programado");
     }
 
     public void conectar(Piano p){
