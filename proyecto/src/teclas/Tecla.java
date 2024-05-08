8d8f4db51b9ea2703f68eafd5214c7b401dae443
niyranzo <yranzo.nicole@gmail.com>
2024-05-08 14:09:45 +0200

    esqueleto tecla, teclafactory

--- proyecto/src/teclas/Tecla.java
+++ proyecto/src/teclas/Tecla.java
@@ -1,4 +1,20 @@
 package teclas;
 
-public class Tecla {
+import java.awt.*;
+
+public abstract class Tecla {
+    protected Point posicion;
+    private int nota;
+    private boolean pulsada;
+    private Color colorPulsada;
+    private Graphics graphics;
+    Tecla(int n){
+
+    }
+    public int getNumeroNota(){
+        return this.nota;
+    }
+    protected abstract int[] getVerticesX();
+    protected abstract int [] getVerticesY();
+
 }
