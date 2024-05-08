9fa6d788e2e61abedd3860c4fbcd6a07eae32ef8
sariasss <sara.arias085@gmail.com>
2024-05-08 14:12:30 +0200

    estructura ElementoVisualTester y pulsable

--- proyecto/src/teclas/ElementoVisualTester.java
+++ proyecto/src/teclas/ElementoVisualTester.java
@@ -1,4 +1,23 @@
 package teclas;
 
+import java.awt.*;
+
 public class ElementoVisualTester {
+    private Graphics graphics;
+    private ElementoVisual elemento;
+
+    public ElementoVisualTester(Graphics g, ElementoVisual e){
+        throw new UnsupportedOperationException("no programado");
+    }
+    public ElementoVisualTester(ElementoVisual e){
+        throw new UnsupportedOperationException("no programado");
+    }
+    public void hacerPrueba(){
+        throw new UnsupportedOperationException("no programado");
+    }
+
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException("no programado");
+    }
 }
+
--- proyecto/src/teclas/Pulsable.java
+++ proyecto/src/teclas/Pulsable.java
@@ -1,4 +1,15 @@
 package teclas;
 
+import java.awt.*;
+
 public interface Pulsable {
+    public void pulsar();
+    public void soltar();
+    public boolean estaPulsado();
+    public void setColorPulsado();
+    public Color getColorPulsado();
+    public Color getColorNoPulsado();
+    public default Color getColor(){
+        throw new UnsupportedOperationException("no programado");
+    }
 }
