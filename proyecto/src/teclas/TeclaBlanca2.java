dc536b4ba3b5ba044f4d28ba0382a0dbeda4c89d
anagg07 <ana24garcia07@gmail.com>
2024-05-09 09:38:00 +0200

    Finalizado paquete programa

--- proyecto/src/teclas/TeclaBlanca2.java
+++ proyecto/src/teclas/TeclaBlanca2.java
@@ -1,5 +1,18 @@
 package teclas;
 
 public class TeclaBlanca2 extends TeclaBlanca{
-    TeclaBlanca(int n){};
+
+    TeclaBlanca2(int n){
+        super(n);
+    };
+
+    @Override
+    protected int[] getVerticesX() {
+        return new int[0];
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        return new int[0];
+    }
 }
